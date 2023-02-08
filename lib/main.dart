23df7270361eb46757ff04f674fe02c20fddda39
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-02-08 14:31:46 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -34,27 +34,36 @@ class MyApp extends StatelessWidget {
           children: [
             ImageHeader(
               profileURL: "https://picsum.photos/id/99/200",
-              name: faker.person.name(),
+              name: faker.person.firstName(),
               city: faker.address.city(),
             ),
-            ImgContainer(imageURL: "https://picsum.photos/id/26/400/300"),
+            ImgContainer(imageURL: "https://picsum.photos/id/26/1200/900"),
             PostButton(),
+            LikedBy(faker: faker),
+            //Caption container
+            CaptionContainer(),
             Divider(color: Colors.grey,),
             ImageHeader(
               profileURL: "https://picsum.photos/id/77/200",
-              name: faker.person.name(),
+              name: faker.person.firstName(),
               city: faker.address.city(),
             ),
-            ImgContainer(imageURL: "https://picsum.photos/id/22/400/300",),
+            ImgContainer(imageURL: "https://picsum.photos/id/22/1200/900",),
             PostButton(),
+            LikedBy(faker: faker),
+            //Caption container
+            CaptionContainer(),
             Divider(color: Colors.grey,),
             ImageHeader(
               profileURL: "https://picsum.photos/id/33/200",
-              name: faker.person.name(),
+              name: faker.person.firstName(),
               city: faker.address.city(),
             ),
-            ImgContainer(imageURL: "https://picsum.photos/id/200/400/300",),
+            ImgContainer(imageURL: "https://picsum.photos/id/200/1200/900",),
             PostButton(),
+            LikedBy(faker: faker),
+            //Caption container
+            CaptionContainer(),
             Divider(color: Colors.grey,),
           ],
         ),
@@ -63,20 +72,72 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class PostButton extends StatelessWidget {
-  const PostButton({
+class CaptionContainer extends StatelessWidget {
+  const CaptionContainer({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        OutlinedButton(onPressed: () {}, child: Icon(Icons.favorite_border_outlined, color: Colors.pink, size: 35,), style: OutlinedButton.styleFrom(backgroundColor: Colors.white, elevation: 0, ),),
-        OutlinedButton(onPressed: () {}, child: Icon(Icons.chat_bubble_outline_outlined, color: Colors.blue, size: 30),style: OutlinedButton.styleFrom(backgroundColor: Colors.white, elevation: 0, ),),
-        OutlinedButton(onPressed: () {}, child: Icon(Icons.bookmark_outline, color: Colors.green, size: 35),style: OutlinedButton.styleFrom(backgroundColor: Colors.white, elevation: 0, ),),
-      ],
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 10, 0, 5),
+      child: Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      ,style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700, ),overflow: TextOverflow.ellipsis, maxLines: 3,  ),
+    );
+  }
+}
+
+class LikedBy extends StatelessWidget {
+  const LikedBy({
+    super.key,
+    required this.faker,
+  });
+
+  final Faker faker;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+      child: Text('Liked by ${faker.person.firstName()} and ${faker.person.firstName()} others', style: TextStyle( fontFamily: 'Montserrat', fontWeight: FontWeight.w800),),
+    );
+  }
+}
+
+class PostButton extends StatefulWidget {
+  const PostButton({
+    super.key,
+  });
+
+  @override
+  State<PostButton> createState() => _PostButtonState();
+}
+
+class _PostButtonState extends State<PostButton> {
+  bool LikePressed = false;
+  bool SavePressed = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          IconButton(onPressed: () {
+            setState(() {
+              LikePressed = !LikePressed;
+            });
+          }, icon: Icon(LikePressed ? Icons.favorite : Icons.favorite_border_outlined, color: LikePressed ? Colors.red : Colors.grey, size: 28,),padding: EdgeInsets.all(10.0),),
+    
+          IconButton(onPressed: () {}, icon: Icon(Icons.chat_bubble_outline, color: Colors.grey, size: 25,),padding: EdgeInsets.all(10.0)),
+    
+          IconButton(onPressed: () {
+            setState(() {
+              SavePressed = !SavePressed;
+            });
+          }, icon: Icon(SavePressed ? Icons.bookmark : Icons.bookmark_outline, color: SavePressed ? Colors.green : Colors.grey, size: 28,),padding: EdgeInsets.all(10.0),)
+        ],
+      ),
     );
   }
 }
@@ -117,6 +178,18 @@ class ImageHeader extends StatelessWidget {
       leading: CircleAvatar(
         backgroundImage: NetworkImage(profileURL),
       ),
+      trailing: OutlinedButton(
+        onPressed: () {
+        },
+        child: Text('Follow', style: TextStyle(color: Colors.black, fontFamily: 'Montserrat', fontWeight: FontWeight.w800),),
+        style: OutlinedButton.styleFrom(
+          backgroundColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          side: BorderSide(color: Colors.grey, width: 1.5)
+        ),
+      )
     );
   }
 }
